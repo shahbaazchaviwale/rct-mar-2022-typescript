00914a99341c6344ca128d91d2ad911543661494
Shahbaaz <shahbaazc@gmail.com>
2022-03-23 14:35:34 +0530

    added props

--- my-app/src/components/TodoItem.tsx
+++ my-app/src/components/TodoItem.tsx
@@ -1,5 +1,10 @@
-const TodoItem: React.FC<{text: string}> = (props) => {
-    return <li>{props.text}</li>
-}
+const TodoItem: React.FC<{ text: string; status: boolean }> = (props) => {
+  return (
+    <li>
+      <p><b>Task Name : </b>{props.text}</p>
+      <p><b>Task Status : </b>{props.status ? "completed" : "In Progress"}</p>
+    </li>
+  );
+};
 
 export default TodoItem;
