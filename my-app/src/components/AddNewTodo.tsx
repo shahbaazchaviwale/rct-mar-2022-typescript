a6a631e6eb8e4622379b9ebc128fe8ba6720b38a
Shahbaaz <shahbaazc@gmail.com>
2022-03-23 15:05:14 +0530

    added useRef()

--- my-app/src/components/AddNewTodo.tsx
+++ my-app/src/components/AddNewTodo.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const submitHandler = (event: React.FormEvent) => {
-  event.preventDefault();
-};
 const AddNewTodo = () => {
+  const getTodoText = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = getTodoText.current?.value;
+    if(!enteredText){
+        return;
+    }
+    console.log('getTodoText >>', enteredText)
+  };
+
   return (
     <form onSubmit={submitHandler}>
       <label>Add Task</label>
-      <input type="text" id="task_text" />
+      <input type="text" id="task_text" ref={getTodoText} />
       <button>Add Todo</button>
     </form>
   );
