d53e85a7295cedfe40f8b33e5e60fbbdbf3fd9ec
Shahbaaz <shahbaazc@gmail.com>
2022-03-23 13:30:28 +0530

    add another component for each item

--- my-app/src/components/Todos.tsx
+++ my-app/src/components/Todos.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Todo from "../modal/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
-    console.log('props >>', props.items);
+  console.log("props >>", props.items);
   return (
     <ul>
-        {props.items.map((item) => (
-            <li key={item.id}>{item.text}</li>
-        ))}  
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
     </ul>
   );
 };
 
 export default Todos;
-
