3b8e7c90c680cdd6a79562b94b5ca6c7e4cafb2e
Shahbaaz <shahbaazc@gmail.com>
2022-03-23 14:35:07 +0530

    add addNewTodo compnent

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,21 @@
-import './App.css';
-import Todos from './components/Todos';
-import Todo from './modal/todo';
-
+import { Fragment } from "react";
+import "./App.css";
+import AddNewTodo from "./components/AddNewTodo";
+import Todos from "./components/Todos";
+import Todo from "./modal/todo";
 
 function App() {
-
-const todosList = [new Todo('React JS', false), new Todo('Angular JS', false), new Todo('Vue JS', false), ]
+  const todosList = [
+    new Todo("React JS", false),
+    new Todo("Angular JS", false),
+    new Todo("Vue JS", false),
+  ];
 
   return (
-    <div>
-      <Todos items={todosList}/>
-    </div>
+    <Fragment>
+      <AddNewTodo />
+      <Todos items={todosList} />
+    </Fragment>
   );
 }
 
